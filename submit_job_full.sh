4b314ffcf39078b9d8b7213c70ace844b38e3d04
Tridib Biswas <tbiswas@cmadrid.hpc.einsteinmed.edu>
2025-06-28 22:51:01 -0400

    different cluster conditions

--- submit_job_full.sh
+++ submit_job_full.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=normal
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=16G
-#SBATCH --job-name seg_5
+#SBATCH --job-name seg_0
 #SBATCH --output  /gs/gsfs0/users/tbiswas/bayes_seg/logs/job.%J.out
 #SBATCH --error   /gs/gsfs0/users/tbiswas/bayes_seg/logs/job.%J.err
 #SBATCH --array=1-47
@@ -16,6 +16,6 @@ conda init
 conda activate seg
 
 # run jobs 
-CMDFILE=commands.txt
+CMDFILE=commands_full.txt
 CMD=$(awk "NR==$SLURM_ARRAY_TASK_ID" $CMDFILE)
 $CMD
