92ddf261c26bd820ac65b5abbedba33bfb39c186
Tridib Biswas <tbiswas@cmadrid.hpc.einsteinmed.edu>
2025-07-31 16:20:52 -0400

    .

--- submit_job_full.sh
+++ submit_job_full.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=normal
 #SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=16G
-#SBATCH --job-name seg_0
+#SBATCH --mem-per-cpu=32G
+#SBATCH --job-name seg_5
 #SBATCH --output  /gs/gsfs0/users/tbiswas/bayes_seg/logs/job.%J.out
 #SBATCH --error   /gs/gsfs0/users/tbiswas/bayes_seg/logs/job.%J.err
 #SBATCH --array=1-47
