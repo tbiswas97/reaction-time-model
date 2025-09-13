eec4aa204276a29637e7f9fb23f899c7a9f08b90
Tridib Biswas <tbiswas@cmadrid.hpc.einsteinmed.edu>
2025-09-12 23:36:54 -0400

    modified jobs files

--- submit_job.sh
+++ submit_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=normal
 #SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=16G
-#SBATCH --job-name bl5_rand
+#SBATCH --mem-per-cpu=32G
+#SBATCH --job-name k2
 #SBATCH --output  /gs/gsfs0/users/tbiswas/bayes_seg/logs/job.%J.out
 #SBATCH --error   /gs/gsfs0/users/tbiswas/bayes_seg/logs/job.%J.err
-#SBATCH --array=1-47
+#SBATCH --array=1-17
 
 
 # start from launch dir
@@ -16,6 +16,6 @@ conda init
 conda activate seg
 
 # run jobs 
-CMDFILE=commands.txt
+CMDFILE=commands_k2.txt
 CMD=$(awk "NR==$SLURM_ARRAY_TASK_ID" $CMDFILE)
 $CMD
