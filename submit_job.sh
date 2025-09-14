ddf3bd37bf28ecabb3f4953781f8592b64b43789
Tridib Biswas <tbiswas@cmadrid.hpc.einsteinmed.edu>
2025-09-14 19:59:32 -0400

    .

--- submit_job.sh
+++ submit_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=normal
 #SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=32G
-#SBATCH --job-name k2
+#SBATCH --mem-per-cpu=8G
+#SBATCH --job-name pmaps
 #SBATCH --output  /gs/gsfs0/users/tbiswas/bayes_seg/logs/job.%J.out
 #SBATCH --error   /gs/gsfs0/users/tbiswas/bayes_seg/logs/job.%J.err
-#SBATCH --array=1-17
+#SBATCH --array=1-79
 
 
 # start from launch dir
@@ -16,6 +16,6 @@ conda init
 conda activate seg
 
 # run jobs 
-CMDFILE=commands_k2.txt
+CMDFILE=commands_get_pmap.txt
 CMD=$(awk "NR==$SLURM_ARRAY_TASK_ID" $CMDFILE)
 $CMD
