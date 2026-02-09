d81f3062fa54bf35c9878fbedb4c396465af8b39
Tridib Biswas <tb@Tridibs-MacBook-Pro.local>
2026-02-09 17:36:57 -0500

    posterior g class

---
+++ Posterior.py
@@ -0,0 +1,128 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+sys.path.append("../sun/src")
+sys.path.append("../em_iteration_analysis/")
+import import_utils as io
+import Response
+from scipy.special import gamma
+import toolbox as tb
+
+class Posterior:
+    def __init__(self,ResponseObj,n_r=500):
+        R = ResponseObj
+        self.points = R.points
+        self.Model = R.Models[0]
+        self.T = self.Model.means_t.shape[0]
+        self.K = self.Model.means_t.shape[1]
+        self.means_t = self.Model.means_t[:,:,6]
+        self.vars_t = self.Model.covars_t[:,:,6,6]
+        self.dofs_t = self.Model.degrees_t
+        
+        self.n_r = n_r
+        self.r = np.linspace(0,10,self.n_r)  
+
+    def analytical_mean(self,point_idx,freeze_params=False):
+        coord = self.points[point_idx]
+
+        x = self.Model.data_pca[coord[0],coord[1],6]
+
+        beta = self.dofs_t/2
+
+        x = x - self.means_t
+
+        gamma_term = gamma((self.dofs_t/2) + 1)/gamma((self.dofs_t/2) + 1/2)
+
+        out = (((beta/x**2) + (1/(2*self.vars_t)))**(-1/2))*gamma_term
+
+        pis = self.Model.weights_t[:,coord[0],coord[1],:]
+
+        if freeze_params:
+            out = (pis@out.T)[:,-1]
+        else:
+            out = np.diag(pis@out.T)
+
+        return out
+
+    def pointwise_posterior(self,point_idx,freeze_params=False):
+        coord = self.points[point_idx]
+        out = np.zeros((self.n_r,self.T))
+        if freeze_params: 
+            vec = self._pointwise_posterior_t(point_idx,self.T-1)
+            for t in range(self.T):
+                pis = self.Model.weights_t[t,coord[0],coord[1],:]
+                out[:,t] = pis@vec.T
+
+        else: 
+            for t in range(self.T):
+                vec = self._pointwise_posterior_t(point_idx,t)
+                pis = self.Model.weights_t[t,coord[0],coord[1],:]
+                out[:,t] = pis@vec.T
+
+        return out
+        
+    def _pointwise_posterior_t(self,point_idx,t):
+        coord = self.points[point_idx]
+        x = self.Model.data_pca[coord[0],coord[1],6]
+
+        p_r_cond_x = np.zeros((self.n_r,self.K))
+        
+        for k in range(self.K):
+            p_r_cond_x[:,k] = np.asarray(
+                [self._f_r_cond_x(
+                    r,
+                    x,
+                    self.means_t[t,k],
+                    self.vars_t[t,k],
+                    self.dofs_t[t,k]
+                )
+                for r in self.r]
+        )
+        
+        return p_r_cond_x
+
+    def _coord_posterior(self,coord):
+        out = np.zeros((self.n_r,self.T))
+        for t in range(self.T):
+            vec = self._coord_posterior_t(coord,t)
+            pis = self.Model.weights_t[t,coord[0],coord[1],:]
+            out[:,t] = pis@vec.T
+
+        return out
+
+    def _coord_posterior_t(self,coord,t):
+        x = self.Model.data_pca[coord[0],coord[1],6]
+
+        p_r_cond_x = np.zeros((self.n_r,self.K))
+        
+        for k in range(self.K):
+            p_r_cond_x[:,k] = np.asarray(
+                [self._f_r_cond_x(
+                    r,
+                    x,
+                    self.means_t[t,k],
+                    self.vars_t[t,k],
+                    self.dofs_t[t,k]
+                )
+                for r in self.r]
+        )
+        
+        return p_r_cond_x
+        
+
+    def _f_r_cond_x(self,r,x,_mu,_var,_dof):
+        g = r
+        alpha = _dof/2
+        beta = _dof/2
+        x = x-_mu
+    
+        g_term = (np.abs(g))**(2*alpha)
+        exp_constant = (beta/np.abs(x)**2)+(1/(2*_var))
+    
+        numer = (exp_constant**(alpha+0.5)) * g_term * np.exp(-(g**2)*exp_constant)
+    
+        denom = gamma(alpha+0.5)
+    
+        y = numer/denom
+
+        return y
