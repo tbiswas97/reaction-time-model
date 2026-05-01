945e796e994ab359c4889a12cd6d15ee825335f9
Tridib Biswas <tb@Tridibs-MacBook-Pro.local>
2026-05-01 17:48:51 -0400

    added variance per coord instead of per grid point

--- Posterior.py
+++ Posterior.py
@@ -6,6 +6,7 @@ from scipy.stats import entropy
 import toolbox as tb
 import pandas as pd
 
+
 class Posterior:
     def __init__(self, ResponseObj, n_r=500):
         """
@@ -25,7 +26,7 @@ class Posterior:
         self.r = np.linspace(0, 10, self.n_r)
         self.sc = 1
 
-    def set_spike_count_multiplier(self,c):
+    def set_spike_count_multiplier(self, c):
         self.sc = c
 
     def analytical_mean(self, point_idx, freeze_params=False, mix=True, _map=False):
@@ -47,9 +48,9 @@ class Posterior:
         else:
             if _map:
                 pis = self.Model.weights_t[:, coord[0], coord[1], :]
-                out = np.diag(out[:,pis.argmax(1)])
+                out = np.diag(out[:, pis.argmax(1)])
 
-        return out*self.sc
+        return out * self.sc
 
     def _analytical_mean(self, coord, freeze_params=False, mix=True, _map=False):
         x = self.Model.data_pca[coord[0], coord[1], 6]
@@ -69,9 +70,49 @@ class Posterior:
         else:
             if _map:
                 pis = self.Model.weights_t[:, coord[0], coord[1], :]
-                out = np.diag(out[:,pis.argmax(1)])
+                out = np.diag(out[:, pis.argmax(1)])
 
-        return out*self.sc
+        return out * self.sc
+
+    def _analytical_var(self, coord, freeze_params=False, mix=True, _map=False):
+        x = self.Model.data_pca[coord[0], coord[1], 6]
+        beta = self.dofs_t / 2
+        x = x - self.means_t
+
+        first_term = ((beta / x**2) + (1 / (2 * self.vars_t))) ** (-1)
+        gamma_term1 = gamma((self.dofs_t / 2) + (3 / 2)) / gamma(
+            (self.dofs_t / 2) + (1 / 2)
+        )
+        gamma_term2 = (
+            gamma((self.dofs_t / 2) + 1) / gamma((self.dofs_t / 2) + 1 / 2)
+        ) ** 2
+
+        out = first_term * (gamma_term1 - (gamma_term2)) * (self.sc**2)
+
+        if mix:
+            means = self.analytical_mean(
+                point_idx, freeze_params=freeze_params, mix=False
+            )
+
+            pis = self.Model.weights_t[:, coord[0], coord[1], :]
+
+            if freeze_params:
+                sq_means = (pis @ (means).T[:, -1]) ** 2
+                means_sq = pis @ (means**2).T[:, -1]
+                mean_vars = (pis @ out.T)[:, -1]
+            else:
+                sq_means = (np.diag(pis @ (means).T)) ** 2
+                means_sq = np.diag(pis @ (means**2).T)
+                mean_vars = np.diag(pis @ out.T)
+
+            out = mean_vars + means_sq - sq_means
+        else:
+            if _map:
+                pis = self.Model.weights_t[:, coord[0], coord[1], :]
+                out = np.diag(out[:, pis.argmax(1)])
+
+        return out
+
     def analytical_var(self, point_idx, freeze_params=False, mix=True, _map=False):
         coord = self.points[point_idx]
         x = self.Model.data_pca[coord[0], coord[1], 6]
@@ -86,7 +127,7 @@ class Posterior:
             gamma((self.dofs_t / 2) + 1) / gamma((self.dofs_t / 2) + 1 / 2)
         ) ** 2
 
-        out = first_term * (gamma_term1 - (gamma_term2))*(self.sc**2)
+        out = first_term * (gamma_term1 - (gamma_term2)) * (self.sc**2)
 
         if mix:
             means = self.analytical_mean(
@@ -96,19 +137,19 @@ class Posterior:
             pis = self.Model.weights_t[:, coord[0], coord[1], :]
 
             if freeze_params:
-                sq_means = (pis@(means).T[:,-1])**2
-                means_sq = pis@(means**2).T[:,-1]
+                sq_means = (pis @ (means).T[:, -1]) ** 2
+                means_sq = pis @ (means**2).T[:, -1]
                 mean_vars = (pis @ out.T)[:, -1]
             else:
-                sq_means = (np.diag(pis@(means).T))**2
-                means_sq = np.diag(pis@(means**2).T)
+                sq_means = (np.diag(pis @ (means).T)) ** 2
+                means_sq = np.diag(pis @ (means**2).T)
                 mean_vars = np.diag(pis @ out.T)
 
-            out = mean_vars + means_sq - sq_means 
+            out = mean_vars + means_sq - sq_means
         else:
-            if _map: 
+            if _map:
                 pis = self.Model.weights_t[:, coord[0], coord[1], :]
-                out = np.diag(out[:,pis.argmax(1)])
+                out = np.diag(out[:, pis.argmax(1)])
 
         return out
 
@@ -198,16 +239,30 @@ class Posterior:
         d = {}
         R = self.R
 
-        d["k"] = [R.kSeg]*len(R.points)*self.T
-        d["img"] = [R.fileinfo['img']]*len(R.points)*self.T
-        d["sub"] = [R.fileinfo["subject"]]*len(R.points)*self.T
-        d["t"] = list(range(self.T))*len(R.points)
+        d["k"] = [R.kSeg] * len(R.points) * self.T
+        d["img"] = [R.fileinfo["img"]] * len(R.points) * self.T
+        d["sub"] = [R.fileinfo["subject"]] * len(R.points) * self.T
+        d["t"] = list(range(self.T)) * len(R.points)
         d["point_idx"] = np.asarray(range(len(R.points))).repeat(self.T)
-        d["mu_mixed"] = np.concatenate([self.analytical_mean(i,mix=True,_map=False) for i in range(len(R.points))])
-        d["mu_MAP"] =  np.concatenate([self.analytical_mean(i,mix=False,_map=True) for i in range(len(R.points))])
-        d["var_mixed"] = np.concatenate([self.analytical_var(i,mix=True,_map=False) for i in range(len(R.points))])
-        d["var_MAP"] =  np.concatenate([self.analytical_var(i,mix=False,_map=True) for i in range(len(R.points))])
-        
+        d["mu_mixed"] = np.concatenate(
+            [
+                self.analytical_mean(i, mix=True, _map=False)
+                for i in range(len(R.points))
+            ]
+        )
+        d["mu_MAP"] = np.concatenate(
+            [
+                self.analytical_mean(i, mix=False, _map=True)
+                for i in range(len(R.points))
+            ]
+        )
+        d["var_mixed"] = np.concatenate(
+            [self.analytical_var(i, mix=True, _map=False) for i in range(len(R.points))]
+        )
+        d["var_MAP"] = np.concatenate(
+            [self.analytical_var(i, mix=False, _map=True) for i in range(len(R.points))]
+        )
+
         entropies = entropy(R.fit_pmap)
         entropy_flat = entropies.T.ravel()
 
